4e81eb036c13255277ad82491a99f91bf143061f
Atharva Bhavsar <atharvabhavsar01@gmail.com>
2022-08-08 15:42:30 +0530

    all files uploded first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Entry from "./Entry.jsx";
+import emojipedia from "../emojipedia";
+
+function createCard(emojicard) {
+  return (
+    <Entry
+      key={emojicard.id}
+      name={emojicard.name}
+      emoji={emojicard.emoji}
+      meaning={emojicard.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">
+        {emojipedia.map(createCard)}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/emojipedia.js
@@ -0,0 +1,47 @@
+import React from "react";
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "🤗",
+    name: "Hugging Face",
+    meaning:
+      "Smiley with red cheeks is stretching out its hands toward you. Wants to hug you. It's an open and cordial gesture and an expression of warmth and friendliness.",
+  },
+  {
+    id: 5,
+    emoji: "❤️",
+    name: "Red Heart",
+    meaning:
+      "The red heart is the classical symbol of love. Expression of passion and romance. But also in the non-romantic context for friendship and strong bonds.",
+  },
+  {
+    id: 6,
+    emoji: "🐸",
+    name: "Frog Face",
+    meaning:
+      " I have a frog in the throat! In mythology, stands for fertility, death and rebirth. Can also be seen in connection with prince charming (kiss the frog). Also known as Kermit, the frog from the Muppet Show.",
+  }
+];
+
+export default emojipedia;
